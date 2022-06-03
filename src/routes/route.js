01bed770561788e797feb8e5b26c1c9864974e4a
anirudh sagar <you@example.com>
2022-06-04 00:28:28 +0530

    sagar

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,94 @@
+//-------Movies-1--------
 const express = require('express');
 const router = express.Router();
+router.get('/movies', function (req, res) {
+    const movies=['Rang de basanti', 'The shining', 'Lord of the rings','Batman begins']
+    res.send(movies)
+});
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+router.get('/movies/:indexNumber', function (req, res) {
+    const movies=['Rang de basanti', 'The shining', 'Lord of the rings','Batman begins']
+    JSON.stringify(req.params)
+    let len = movies.length
+    let item = movies[req.params.indexNumber]
+    if(req.params.indexNumber < len){
+        res.send(item)
+    }else{
+        res.send('Error: There is no movie.')
+    }
+    
+    //console.log(req.params.indexNumber)
+});
+
+router.get('/films', function (req, res) {
+    const film = [{
+           id: 1,
+           name: "The Shining"
+          },
+          {
+           id: 2,
+           name: "Incendies"
+          },
+          {
+            id: 3,
+            name: "Rang de Basanti"
+          },
+          {
+           id: 4,
+           name: "Finding Nemo"
+          }];
+       
+       res.send(film)
+});
+
+router.get('/film/:filmId', function (req, res) {
+    const films = [{
+        id: 1,
+        name: "The Shining"
+       },
+       {
+        id: 2,
+        name: "Incendies"
+       },
+       {
+         id: 3,
+         name: "Rang de Basanti"
+       },
+       {
+        id: 4,
+        name: "Finding Nemo"
+       }
+    ]
+   // console.log(films[0])
+       JSON.stringify(req.params)
+       const len = films.length
+       const item = films[req.params.filmId]
+       if(req.params.filmId < len){
+          res.send(item)
+       }else{
+        res.send('Error: There is no movie.')
+      }
+      //res.send("adfdfa")
+});
+
+
+router.get('/candidates', function(req, res){
+    console.log('Query paramters for this request are '+JSON.stringify(req.query))
+    let gender = req.query.gender
+    let state = req.query.state
+    let district = req.query.district
+    console.log('State is '+state)
+    console.log('Gender is '+gender)
+    console.log('District is '+district)
+    let candidates = ['Akash','Suman']
+    res.send(candidates)
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
+router.get('/candidates/:canidatesName', function(req, res){
+    console.log('The request objects is '+ JSON.stringify(req.params))
+    console.log('Candidates name is '+req.params.canidatesName)
+    res.send('Done')
 })
 
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
 
 module.exports = router;
